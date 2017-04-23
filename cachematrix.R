e440ec4632779bae57ace697a9e1637535df0c56
Niikola-Andrii <texhika.2020@gmail.com>
2017-04-23 15:51:35 -0500

    Finished Assignment-2 for R-Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,61 @@
-## Put comments here that give an overall description of what your
-## functions do
+# The two functions together provide a means to cache data 
+# rather then recompute the data. The functions here are 
+# almost identical to those used in the "Caching the Mean"
+# example given in Programming Assignment 2 description.
 
-## Write a short comment describing this function
+# Creates a list containing functions to:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of the inverse of matrix.
+# 4. get the value of the inverse of matrix.
+makeCacheMatrix <- function(x = matrix()) 
+    {
+    m <- NULL
+    
+    set <- function(y) {x <<- y; m <<- NULL}
+    get <- function() x
+    
+    setSolve <- function(solve) m <<- solve
+    getSolve <- function() m
+    
+    list(set = set,get = get, setSolve = setSolve, getSolve = getSolve)
+    }
 
-makeCacheMatrix <- function(x = matrix()) {
+# The following function calculates the inverse of 
+# matrix created with the above function. However, 
+# it first checks to see if the inverse has
+# already been calculated.
+cacheSolve <- function(x, ...) 
+    {
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getSolve()
 
-}
+    if(!is.null(m))
+        {
+        message("2. GET cached data")
+        return(m)
+        }
+  
+    message("1. SET cached data")
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setSolve(m);
+    m;
+    }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# a Test Run
+# ----------
+#
+# > a = makeCacheMatrix(matrix(c(1,2,1,2,5,0,3,3,8),nrow = 3,ncol = 3))
+# > cacheSolve(a)
+# 1. SET cached data
+#       [,1] [,2] [,3]
+# [1,]  -40   16    9
+# [2,]   13   -5   -3
+# [3,]    5   -2   -1
+# > cacheSolve(a)
+# 2. GET cached data
+#       [,1] [,2] [,3]
+# [1,]  -40   16    9
+# [2,]   13   -5   -3
+# [3,]    5   -2   -1
